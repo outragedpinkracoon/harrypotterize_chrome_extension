ae3ffb398329ec50d96ab8af44a5f3f113e1bdad
Callum Hogg <bananaCypher@gmail.com>
2016-08-01 01:01:16 +0100

    #Replace names
    * Added name_replace.js file to handle replacing
    all the names in the content with Harry Potter
    characters
    * Added a characters.js file to hold a list of
    characters that can be used for replacement
    * Updated the manifest file to include the 2 new
    JS files

---
+++ code/characters.js
@@ -0,0 +1,32 @@
+var characters = [
+  {
+    full: 'Harry Potter',
+    first: 'Harry',
+    last: 'Potter'
+  },
+  {
+    full: 'Ron Weasley',
+    first: 'Ron',
+    last: 'Weasley'
+  },
+  {
+    full: 'Hermione Granger',
+    first: 'Hermione',
+    last: 'Granger'
+  },
+  {
+    full: 'Lord Voldemort',
+    first: 'Lord Voldemort',
+    last: 'Voldemort'
+  },
+  {
+    full: '"he who shall not be named"',
+    first: '"he who shall not be named"',
+    last: '"he who shall not be named"'
+  },
+  {
+    full: 'Lucius Malfoy',
+    first: 'Lucius',
+    last: 'Malfoy'
+  }
+]
